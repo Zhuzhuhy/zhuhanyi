34a60d6b419990b3fb14e83192253db2aaa8a4b6
tracer-ics2015 <tracer@njuics.org>
2017-02-25 12:03:25 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  12:03:25 up  1:11,  1 user,  load average: 0.11, 0.03, 0.01 3660102146331d13ebb9a592e67d030af1840cd8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -85,16 +85,17 @@ static int cmd_info(char *args) {
 
 static int cmd_x(char *args){
      char *locate_start,*locate_len;
-	 int  num,count = 0,locate,num_s;
+	 int  num, num_s;
 	 locate_len = strtok(args," ");
 	 locate_start = strtok(args," ");
      num = atoi(locate_len);
 	 num_s = atoi(locate_start);
  	/* while(count < num){*/
-     locate = swaddr_read(num_s,num + count);
+/*     locate = swaddr_read(num_s,num + count);
 	 printf("%02x",locate);
-	/* count++;
+	 count++;
 	 }*/
+	 printf("%d\t%02x", num,num_s);
 	 return 0;
 }
 
