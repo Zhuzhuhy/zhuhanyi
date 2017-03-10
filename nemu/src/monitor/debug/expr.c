dd5c400fed762897140c85970eb13ec802800b7b
tracer-ics2015 <tracer@njuics.org>
2017-03-10 20:01:07 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  20:01:07 up 38 min,  1 user,  load average: 0.00, 0.05, 0.07 70c9c75926d134ffd4d745a5460fa487e59fdd84

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -84,11 +84,12 @@ static bool make_token(char *e) {
 
 			Log("\nmatch rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
+			
   				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array ``tokens''. For certain 
 				 * types of tokens, some extra actions should be performed.
  			 	 */ 
-
+             printf("%d",rules[i].token_type);
  				switch(rules[i].token_type) {
 					case '+':
 					tokens[nr_token].type = '+';
@@ -120,8 +121,9 @@ static bool make_token(char *e) {
    		 		}
 
 				break;
-    			} 
+    			
         nr_token ++;
+			}
  		}
  
    		if(i == NR_REGEX) {
