00f9c2b3d125673ed1ff0ea64a998096a2bc5786
tracer-ics2015 <tracer@njuics.org>
2017-03-12 02:01:16 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  02:01:16 up 15:31,  1 user,  load average: 1.06, 1.01, 1.00 cb26dae990e7a52bed30fa139f2a5a47e2564915

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -278,7 +278,7 @@ uint32_t eval(int p,int q){
 		int op;
 		int val1,val2,val3;
 		op = dominant(p , q);
-		printf("%d\n",op);
+		
 		if(op == 0){
 		val3 = eval(op+1,q);
 		switch(tokens[op].type)
@@ -301,7 +301,7 @@ uint32_t eval(int p,int q){
 		if(op != 0){
 		val1 = eval(p , op - 1);
 		val2 = eval(op + 1, q);
-		printf("%d %d\n",val1,val2);
+	
  	 	switch(tokens[op].type){
 			case '+':return  val1 + val2; 
 			case '-':return  val1 - val2;
