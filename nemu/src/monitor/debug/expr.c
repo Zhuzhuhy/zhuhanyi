64820a78b5a0351c4fb351171df2cf35ce4bace5
tracer-ics2015 <tracer@njuics.org>
2017-03-11 19:58:05 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  19:58:05 up  9:28,  1 user,  load average: 1.07, 1.02, 0.92 b612ad3142466c02ed9a2584638cb67181a3cad2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -149,25 +149,15 @@ static bool make_token(char *e) {
 
  bool check_parentheses(p,q){
    if(tokens[p].type == '(' && tokens[q].type == ')') return true;
-   /*    while(p<=q){
-    if(tokens[p].type == '(' ) i++;
-	if(tokens[p].type == '(' && p != 0 && i == 0) return false; 
-	if(tokens[p].type == ')') i--;
-	if(tokens[p].type == ')' && i == 0)  return false;
-	else j++;
-    p++;
-    }
-	
-   if(i == 0 && j-1 != q) return true;
-  */ else return false;
+   else return false;
 }
 int dominant(int p,int q){
    int i = p,j=0;
    int max = 0;
-       while(p<=q){
-         if(tokens[p].type == '('){
+        while(p<=q){
+          if(tokens[p].type == '('){
     		   j++;p++;
-	         while(p<=q){
+ 	         while(p<=q){
 	   if(tokens[p].type == '(')
 		   j++;
 	   if(tokens[p].type == ')')
@@ -175,19 +165,21 @@ int dominant(int p,int q){
 	   if(j == 0) break;
 	   p++; 
 	   
-	   }
- 	   }
-	   if(tokens[p].type == ')')
-		     p++;
+	   } 
+ 	   } 
+	   if(tokens[p].type == ')')   p++;
        if(tokens[p].type != dec && tokens[p].type != hex && tokens[p].type != '(' && tokens[p].type != ')'){
 	     max = p;
-        for(i=p;i<= q;i++){
+         for(i=p;i<= q;i++){
         if(tokens[i].type >= tokens[max].type && tokens[i].type!=dec && tokens[i].type != hex && tokens[p].type !='('&&tokens[p].type !=')')
             max = i; 
- 	      }
-          }  
-        p++;
+ 	       }
+            }  
+       if(tokens[p].type != dec && tokens[p].type != hex && tokens[p].type == '(' && tokens[p].type == ')'){
+        printf("error");
        }
+	   p++;
+		}
    return max;
    }
 
