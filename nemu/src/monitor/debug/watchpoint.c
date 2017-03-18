ea1c3e80f4cb75524204d2b53c8fa0d176762972
tracer-ics2015 <tracer@njuics.org>
2017-03-18 10:01:30 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  10:01:30 up 37 min,  1 user,  load average: 0.00, 0.00, 0.00 985e107b21b249b1182f1ec2ab98300ee88cd535

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -144,7 +144,7 @@ void list_watchpoint(WP* list){
  bool scan_watchpoint(){
  WP* p;
  p = head;
- printf("yuio");
+if(head == NULL) printf("yuio");
  printf("\n%s",head->e);
   while(p){
  if(p->e != NULL)
