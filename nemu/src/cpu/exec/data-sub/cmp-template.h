c227a789062bbf7f75141891ce55342f5b893077
tracer-ics2015 <tracer@njuics.org>
2017-04-30 12:15:32 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  12:15:32 up  3:44,  1 user,  load average: 0.09, 0.03, 0.01 5ea03326efc2bd63eb6611ffcd6d179a9dd89fc2

--- nemu/src/cpu/exec/data-sub/cmp-template.h
+++ nemu/src/cpu/exec/data-sub/cmp-template.h
@@ -4,8 +4,8 @@
 static void do_execute(){
   DATA_TYPE val;
   val = op_dest->val-op_src->val;
-
-  if(op_dest->val < op_src->val) 
+ /*
+  if(val < op_dest->val) 
 	  cpu.EFLAGS.CF = 1;
   else cpu.EFLAGS.CF = 0;
 
@@ -30,10 +30,29 @@ static void do_execute(){
   }  
   if(j%2==0) cpu.EFLAGS.PF = 1;
   else cpu.EFLAGS.PF = 0;
-  
-}
-make_instr_helper(si2rm)
-make_instr_helper(r2rm)
-make_instr_helper(rm2r)
+ */
+ int len=(DATA_BYTE<<3)-1;
+ int s1,s2;
+ cpu.EFLAGS.CF=op_src->val<op_dest->val;
+ cpu.EFLAGS.SF=val>>len;
+ s1=op_dest->val>>len;
+ s2=op_src->val>>len;
+ cpu.EFLAGS.OF=(s1==s2&&s1!=cpu.EFLAGS.SF);
+ cpu.EFLAGS.ZF=!val;
+ val^=val>>4;
+ val^=val>>2;
+ val^=val>>1;
+ cpu.EFLAGS.PF=!(val&1);
 
+
+ // OPERAND_W(op_dest, val);
+  print_asm_template2();
+}
+//#if DATA_TYPE==2||DATA_TYPE==4
+make_instr_helper(si2rm)
+//#endif
+
+//make_helper(cmp_si2rm_v);
+make_instr_helper(r2rm);
+make_instr_helper(rm2r);
 #include "cpu/exec/template-end.h"
