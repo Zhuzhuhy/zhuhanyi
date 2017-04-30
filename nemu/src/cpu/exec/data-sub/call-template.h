bf8e532c812dfc2186ff6f632351627bf7d698a1
tracer-ics2015 <tracer@njuics.org>
2017-04-30 09:59:48 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  09:59:48 up  1:28,  1 user,  load average: 0.07, 0.02, 0.00 9e5a8dc325f96c209749d07927a98383b6088a08

--- nemu/src/cpu/exec/data-sub/call-template.h
+++ nemu/src/cpu/exec/data-sub/call-template.h
@@ -3,6 +3,31 @@
 #define instr call
 
 static void do_execute(){
+  if(op_src->type==OP_TYPE_REG||op_src->val==OP_TYPE_MEM)
+  {
+	  uint32_t eip=cpu.eip;
+	  REG(R_ESP)-=DATA_BYTE;
+	  cpu.eip=op_src->val-2;
+	  if(DATA_BYTE==2)
+		  cpu.eip=cpu.eip&0xffff;
+	  int l=concat(decode_rm_,SUFFIX)(eip+1);
+	  MEM_W(REG(R_ESP),eip+l+1);
+	  }
+  if(op_src->type==OP_TYPE_IMM)
+  { 
+   REG(R_ESP)-=DATA_BYTE;
+#if DATA_BYTE==2
+   swaddr_write(REG(R_ESP),DATA_BYTE,(cpu.eip+DATA_BYTE+1)&0xffff);
+   cpu.eip=cpu.eip+op_src->val;
+   cpu.eip=cpu.eip&0xffff;
+#endif
+
+#if DATA_BYTE==4
+   swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+DATA_BYTE+1);
+   cpu.eip=cpu.eip+op_src->val;
+#endif
+  }
+  print_asm_template1();
 }
 
 make_instr_helper(i);
