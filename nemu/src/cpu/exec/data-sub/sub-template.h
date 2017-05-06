0ad630a6f142085576ab9f218a8eb3150b6415a3
tracer-ics2015 <tracer@njuics.org>
2017-05-06 13:41:31 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  13:41:31 up  2:11,  1 user,  load average: 0.15, 0.04, 0.01 c40b84fbc4128fe5d4af353e7269cff613f5925e

--- nemu/src/cpu/exec/data-sub/sub-template.h
+++ nemu/src/cpu/exec/data-sub/sub-template.h
@@ -2,22 +2,28 @@
 
 #define instr sub
 static void do_execute(){
-  DATA_TYPE val;
-  val = op_dest->val - op_src->val;
-  /*cpu.EFLAGS.CF=op_dest->val<op_src->val;
+  DATA_TYPE result;
+  result = op_dest->val - op_src->val;
+  cpu.EFLAGS.CF=op_dest->val<op_src->val;
   cpu.EFLAGS.SF=(result>>31)&1;
-  int i,count=0,flag;
+/*  int i,count=0,flag;
   uint32_t p=(result&255);
   for(i=0;i<8;i++){
    flag=p&1;
    if(flag==1)count++;
    p=p>>1;
   }
-  cpu.EFLAGS.OF=((op_dest->val>>31)^(op_src->val>>31))&&(!((result>>31)^(op_src->val>>31)));
-  cpu.EFLAGS.PF=(count%2==0);
+  */
+  cpu.EFLAGS.OF=((op_dest->val>>31)^(op_src->val>>31))&((op_dest->val>>31)^(result>>31))&1;
+  uint32_t pf=(result&255);
+  pf=(pf>>4)&pf;
+  pf=(pf>>2)&pf;
+  pf=(pf>>1)&pf;
+
+  cpu.EFLAGS.PF=(pf&1);
   cpu.EFLAGS.ZF=(result==0);
   
-*/  
+/*  
   if(op_dest->val < op_src->val) 
 	  cpu.EFLAGS.CF = 1;
   else cpu.EFLAGS.CF = 0;
@@ -26,10 +32,10 @@ static void do_execute(){
 	  cpu.EFLAGS.ZF = 1;
   else cpu.EFLAGS.ZF = 0;
 
-  /*if(op_dest->val*op_src->val <0 && op_src->val*val>=0)
+  if(op_dest->val*op_src->val <0 && op_src->val*val>=0)
 	  cpu.EFLAGS.OF = 1;
   else cpu.EFLAGS.OF=0;
-*/
+
   cpu.EFLAGS.OF=((op_dest->val>>31)^(op_src->val>>31))&&(!((val>>31)^(op_src->val>>31)));
 //  cpu.EFLAGS.PF=(count%2==0);
   if(val<0)
@@ -38,7 +44,7 @@ static void do_execute(){
 
   int i =1,j=0;
   DATA_TYPE num = val;
-  while(i<=8){
+   while(i<=8){
   if(MSB(num) == 1) j++;
   num = num << 1;
   i++;
@@ -46,8 +52,8 @@ static void do_execute(){
   if(j%2==0) cpu.EFLAGS.PF = 1;
   else cpu.EFLAGS.PF = 0;
  
-
-  OPERAND_W(op_dest, val);
+*/
+  OPERAND_W(op_dest, result);
   print_asm_template2();
 }
 make_instr_helper(si2rm)
