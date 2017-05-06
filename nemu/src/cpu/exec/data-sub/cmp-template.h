63b22271510a802d6b492b3029d13ebff2cef778
tracer-ics2015 <tracer@njuics.org>
2017-05-06 12:19:06 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  12:19:06 up 49 min,  1 user,  load average: 0.08, 0.02, 0.01 a39728b8adca56eb363973d6a4f26da2fa03474d

--- nemu/src/cpu/exec/data-sub/cmp-template.h
+++ nemu/src/cpu/exec/data-sub/cmp-template.h
@@ -3,9 +3,22 @@
 #define instr cmp
 static void do_execute(){
   DATA_TYPE val;
-  val = op_dest->val-op_src->val;
- 
-  if(op_dest->val < op_dest->val) 
+  val = op_dest->val - op_src->val;
+  /*cpu.EFLAGS.CF=op_dest->val<op_src->val;
+  cpu.EFLAGS.SF=(result>>31)&1;
+  int i,count=0,flag;
+  uint32_t p=(result&255);
+  for(i=0;i<8;i++){
+   flag=p&1;
+   if(flag==1)count++;
+   p=p>>1;
+  }
+  cpu.EFLAGS.OF=((op_dest->val>>31)^(op_src->val>>31))&&(!((result>>31)^(op_src->val>>31)));
+  cpu.EFLAGS.PF=(count%2==0);
+  cpu.EFLAGS.ZF=(result==0);
+  
+*/ 
+  if(op_dest->val < op_src->val) 
 	  cpu.EFLAGS.CF = 1;
   else cpu.EFLAGS.CF = 0;
 
@@ -13,12 +26,12 @@ static void do_execute(){
 	  cpu.EFLAGS.ZF = 1;
   else cpu.EFLAGS.ZF = 0;
 
+  /*if(op_dest->val*op_src->val <0 && op_src->val*val>=0)
+	  cpu.EFLAGS.OF = 1;
+  else cpu.EFLAGS.OF=0;
+*/
   cpu.EFLAGS.OF=((op_dest->val>>31)^(op_src->val>>31))&&(!((val>>31)^(op_src->val>>31)));
-   //cpu.EFLAGS.PF=(count%2==0);
- // if(op_dest->val*op_src->val <0 && op_src->val*val>=0)
-//	  cpu.EFLAGS.OF = 1;
- // else cpu.EFLAGS.OF=0;
-//
+//  cpu.EFLAGS.PF=(count%2==0);
   if(val<0)
 	  cpu.EFLAGS.SF=1;
   else cpu.EFLAGS.SF = 0;
@@ -33,29 +46,13 @@ static void do_execute(){
   if(j%2==0) cpu.EFLAGS.PF = 1;
   else cpu.EFLAGS.PF = 0;
  
-/* int len=(DATA_BYTE<<3)-1;
- int s1,s2;
- cpu.EFLAGS.CF=op_dest->val<op_src->val;
- cpu.EFLAGS.SF=val>>len;
- s1=op_dest->val>>len;
- s2=op_src->val>>len;
- cpu.EFLAGS.OF=(s1!=s2&&s2==cpu.EFLAGS.SF);
- cpu.EFLAGS.ZF=!val;
- val^=val>>4;
- val^=val>>2;
- val^=val>>1;
- cpu.EFLAGS.PF=!(val&1);
-*/
 
  // OPERAND_W(op_dest, val);
   print_asm_template2();
 }
-//#if DATA_TYPE==2||DATA_TYPE==4
 make_instr_helper(si2rm)
-//#endif
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
 
-//make_helper(cmp_si2rm_v);
-make_instr_helper(r2rm);
-make_instr_helper(i2rm);
-make_instr_helper(rm2r);
 #include "cpu/exec/template-end.h"
