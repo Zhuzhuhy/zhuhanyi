337fc3f5f90ad85c530eaf08f798dc6f3f95eb73
tracer-ics2015 <tracer@njuics.org>
2017-05-06 13:06:30 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  13:06:30 up  1:36,  1 user,  load average: 0.00, 0.00, 0.00 a602d08bec66a092dff2a743e5da8d678c796a

--- nemu/src/cpu/exec/data-sub/sub-template.h
+++ nemu/src/cpu/exec/data-sub/sub-template.h
@@ -17,10 +17,10 @@ static void do_execute(){
   cpu.EFLAGS.PF=(count%2==0);
   cpu.EFLAGS.ZF=(result==0);
   
-*/ 
-//  if(op_dest->val < op_src->val) 
-//	  cpu.EFLAGS.CF = 1;
- // else cpu.EFLAGS.CF = 0;
+*/  
+  if(op_dest->val < op_src->val) 
+	  cpu.EFLAGS.CF = 1;
+  else cpu.EFLAGS.CF = 0;
 
   if(op_dest->val == op_src->val)
 	  cpu.EFLAGS.ZF = 1;
