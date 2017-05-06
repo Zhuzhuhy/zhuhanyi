b8a956b45693c8cad52c8cb2d50062936bf62f4c
tracer-ics2015 <tracer@njuics.org>
2017-05-06 22:21:13 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  22:21:13 up  3:12,  1 user,  load average: 0.00, 0.00, 0.00 1735b9afe12138d72915a25cf54b0f0ed7b51b16

--- nemu/src/cpu/exec/data-mov/jbe-template.h
+++ nemu/src/cpu/exec/data-mov/jbe-template.h
@@ -3,9 +3,9 @@
 #define instr jbe
 
 static void do_execute() {
-
-    if(cpu.EFLAGS.ZF == 1||cpu.EFLAGS.CF==1) {
-		cpu.eip = cpu.eip + op_src->val;
+    DATA_TYPE_S d=op_src->val;
+    if(cpu.EFLAGS.ZF == 1|| cpu.EFLAGS.CF==1) {
+		cpu.eip = cpu.eip + d;
 	   if(DATA_BYTE==2)
 		   cpu.eip&=0xffff;
 	}
