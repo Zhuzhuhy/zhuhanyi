2b72b22a43d2c753f2b3e213b7a6a1b82567cf77
tracer-ics2015 <tracer@njuics.org>
2017-06-03 14:36:47 +0800

    >  compile NEMU 161520304 zhy Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  14:36:47 up  4:19,  1 user,  load average: 0.08, 0.06, 0.04 fcf19d69e1d447ede6707e5bd14333a44e83a19e

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -63,7 +63,8 @@ void CacheWriteByte(hwaddr_t addr, uint8_t data){
 		cb -> data[offset] = data;
 		//dram_write(addr, 1, (uint32_t)data);
 		L2CacheWriteByte(addr, data);
-	}else{
+	}
+	else{
 		//dram_write(addr, 1, (uint32_t)data);
 		L2CacheWriteByte(addr, data);
 	}
@@ -80,7 +81,7 @@ size_t CacheWrite(hwaddr_t addr, CacheBlock *cb, size_t len, uint32_t data){
 void CacheReadByte(hwaddr_t addr, uint8_t *data){
 	CacheBlock *cb = NULL;
 	uint32_t offset = ADDR(addr);
-	if(shot(addr, &cb) == true){
+ 	if(shot(addr, &cb) == true){
 		px+=2;
 		*data = cb -> data[offset];
 	}else{
@@ -88,7 +89,7 @@ void CacheReadByte(hwaddr_t addr, uint8_t *data){
 		cb = CopyToCache(addr);
 		*data = cb -> data[offset];
 	}
-	//printf("---------read addr = 0x%x data = 0x%x\n", addr, *data);
+	printf("---------read addr = 0x%x data = 0x%x\n", addr, *data);
 
 }
 
